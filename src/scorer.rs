5a79432b89e52d9cc9dbcb78a060eee23f4215cd
Ben Newton <ben.fletcher.newton@gmail.com>
2024-12-28 22:27:18 +0000

    Initial commit

---
+++ src/scorer.rs
@@ -0,0 +1,11 @@
+pub struct SudokuScorer;
+
+impl SudokuScorer {
+    pub fn count_duplicates(numbers: &[u8]) -> Vec<(u8, u32)> {
+        // ... duplicate counting logic ...
+    }
+
+    pub fn score(grid: Vec<Vec<u8>>) -> Result<u32, Box<dyn Error>> {
+        // ... scoring logic ...
+    }
+} 
